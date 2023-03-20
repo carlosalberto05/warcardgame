6f91869f63ab4ed2ed24a1de8772b1b1d3a02170
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2023-03-20 12:23:56 -0600

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Carlos Alberto on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
